bcc8684a67d33ca2c1f5842beadb8eafc018878e
Ivan Fernandez OM <ivan.fernandezcampos@optivamedia.com>
2022-11-18 15:11:03 +0100

    chore: discount repository mock

---
+++ src/Contexts/Mooc/discounts/__mocks__/discountRespository.mock.ts
@@ -0,0 +1,39 @@
+import { Nullable } from '../../shared/nullable';
+import Discount from '../domain/discount';
+import DiscountId from '../domain/discountId';
+import discountId from '../domain/discountId';
+import DiscountRepository from '../domain/discountRepository';
+
+export default class DiscountRepositoryMock implements DiscountRepository {
+    private mockSave = jest.fn();
+
+    private mockSearch = jest.fn();
+
+    async save(discount: Discount): Promise<void> {
+        this.mockSave(discount);
+    }
+
+    assertSaveHasBeenCalledWith(discount: Discount): void {
+        const { mock } = this.mockSave,
+            lastSavedDiscount = mock.calls[mock.calls.length - 1][0] as Discount;
+
+        expect(lastSavedDiscount).toBeInstanceOf(Discount);
+        expect(lastSavedDiscount).toStrictEqual(discount);
+    }
+
+    assertNothingSaved(): void {
+        expect(this.mockSave).not.toHaveBeenCalled();
+    }
+
+    search(id: discountId): Promise<Nullable<Discount>> {
+        return this.mockSearch(id);
+    }
+
+    whenSearchThenReturn(discount: Nullable<Discount>): void {
+        this.mockSearch.mockReturnValue(discount);
+    }
+
+    assertSearchHasBeenCalledWith(id: DiscountId): void {
+        expect(this.mockSearch).toHaveBeenCalledWith(id);
+    }
+}
