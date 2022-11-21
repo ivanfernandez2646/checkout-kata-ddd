f3e64767682f303a65cefe2cbbab0dda7a2a0de4
Ivan Fernandez OM <ivan.fernandezcampos@optivamedia.com>
2022-11-21 11:00:48 +0100

    chore: update unit test (discount)

--- src/Contexts/Mooc/discounts/application/create/discountCreator.test.ts
+++ src/Contexts/Mooc/discounts/application/create/discountCreator.test.ts
@@ -1,4 +1,5 @@
 import DiscountMother from '../../domain/discount.mother';
+import DiscountExistsException from '../../domain/discountExistsException';
 import DiscountRepositoryMock from '../../__mocks__/discountRespository.mock';
 import DiscountCreator from './discountCreator';
 
@@ -10,9 +11,27 @@ beforeAll(() => {
     creator = new DiscountCreator(repository);
 });
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 describe('DiscountCreator', () => {
+    it('should throw an error when the discount already exists', async () => {
+        const discount = DiscountMother.random();
+
+        repository.whenSearchThenReturn(discount);
+
+        await expect(
+            creator.run({ id: discount.id.value, threshold: discount.threshold.value, amount: discount.amount.value })
+        ).rejects.toThrow(DiscountExistsException);
+
+        repository.assertNothingSaved();
+    });
+
     it('should create a valid discount', async () => {
         const discount = DiscountMother.random();
+
+        repository.whenSearchThenReturn(null);
 
         await creator.run({ id: discount.id.value, threshold: discount.threshold.value, amount: discount.amount.value });
 
--- src/Contexts/Mooc/discounts/application/find/discountFinder.test.ts
+++ src/Contexts/Mooc/discounts/application/find/discountFinder.test.ts
@@ -1,4 +1,5 @@
 import DiscountMother from '../../domain/discount.mother';
+import DiscountNotFoundException from '../../domain/discountNotFoundException';
 import DiscountRepositoryMock from '../../__mocks__/discountRespository.mock';
 import DiscountFinder from './discountFinder';
 
@@ -10,13 +11,17 @@ beforeAll(() => {
     finder = new DiscountFinder(repository);
 });
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 describe('DiscountCreator', () => {
     it('should throw an error when the discount does not exist', async () => {
         const discount = DiscountMother.random();
 
         repository.whenSearchThenReturn(null);
 
-        await expect(finder.run(discount.id.value)).rejects.toThrowError();
+        await expect(finder.run(discount.id.value)).rejects.toThrow(DiscountNotFoundException);
 
         repository.assertSearchHasBeenCalledWith(discount.id);
     });
